b01ab69c291293ef708eadf747d6595a806f8e24
stuck in code <bonidaniel52@gmail.com>
2026-01-13 15:05:51 +0000

    cron job added

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
-import bookRoutes from "./routes/bookRoutes.js"
+import bookRoutes from "./routes/bookRoutes.js";
 import connectDB from "./config/connect.js";
-import cors from 'cors'
+import cors from "cors";
+import job from "./config/cron.js";
+
 const app = express();
 // how to generate a rendom characters for your secret in "bash terminal"
 // openssl rand -base64 32
 dotenv.config();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-app.use("api/books", bookRoutes)
+app.use("api/books", bookRoutes);
 
 connectDB();
+job.start();
 
 const PORT = process.env.PORT || 8880;
 app.listen(PORT, () => {
